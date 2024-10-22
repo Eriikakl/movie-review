34ffe901fc8312856334a71f9ce13557b83d317d
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-10-22 06:50:26 +0300

    Initial Spring Boot setup with dependencies

---
+++ src/test/java/hh/sof03/moviereview/MoviereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof03.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
