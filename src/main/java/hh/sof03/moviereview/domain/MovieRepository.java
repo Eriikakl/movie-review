7e6ae218689a3e99f051616112babf878a86f7fb
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-10-22 07:41:51 +0300

    First version of CRUD repositories

---
+++ src/main/java/hh/sof03/moviereview/domain/MovieRepository.java
@@ -0,0 +1,7 @@
+package hh.sof03.moviereview.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+
+}
---
+++ src/main/java/hh/sof03/moviereview/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package hh.sof03.moviereview.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
