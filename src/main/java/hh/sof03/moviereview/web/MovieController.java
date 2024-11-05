280a1cfda03fdec545197d834f1ba7e5ec45b359
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-11-05 16:11:35 +0200

    Fixed delete movie and save movie -endpoints

--- src/main/java/hh/sof03/moviereview/web/MovieController.java
+++ src/main/java/hh/sof03/moviereview/web/MovieController.java
@@ -11,12 +11,10 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 import hh.sof03.moviereview.domain.Category;
 import hh.sof03.moviereview.domain.CategoryRepository;
 import hh.sof03.moviereview.domain.Movie;
 import hh.sof03.moviereview.domain.MovieRepository;
-
 
 @Controller
 public class MovieController {
@@ -37,8 +35,8 @@ public class MovieController {
 
     // Add new movie
     // http://localhost:8080/addmovie
-    @PostMapping("/addmovie")
-     public String addMovie(Model model) {
+    @GetMapping("/addmovie")
+    public String addMovie(Model model) {
         model.addAttribute("movie", new Movie());
 
         List<Category> categories = new ArrayList<>();
@@ -47,10 +45,10 @@ public class MovieController {
         }
         model.addAttribute("categories", categories);
         return "addmovie";
-     }
+    }
 
     // Update book
-     // http://localhost:8080/edit/1
+    // http://localhost:8080/edit/1
     @PatchMapping("/edit/{id}")
     public String editMovie(@PathVariable("id") Long movieid, Model model) {
         Movie movie = mrepository.findById(movieid).orElse(null);
@@ -66,15 +64,15 @@ public class MovieController {
     }
 
     // Save added movie
-     // http://localhost:8080/saveadded
-    @GetMapping("/saveadded")
+    // http://localhost:8080/saveadded
+    @PostMapping("/saveadded")
     public String saveAddedMovie(Movie movie) {
         mrepository.save(movie);
         return "redirect:movielist";
     }
 
     // Save updated movie
-     // http://localhost:8080/saveupdated
+    // http://localhost:8080/saveupdated
     @GetMapping("/saveupdated")
     public String saveUpdatedMovie(Movie movie) {
         mrepository.save(movie);
@@ -82,8 +80,9 @@ public class MovieController {
     }
 
     // Delete movie
-     // http://localhost:8080/delete
-    @GetMapping("/delete")
+    // http://localhost:8080/delete/1
+    @GetMapping("/deletemovie/{id}")
+    // @PreAuthorize("hasAuthority('ADMIN')")
     public String deleteMovie(@PathVariable("id") Long movieid, Model model) {
         mrepository.deleteById(movieid);
         return "redirect:../movielist";
